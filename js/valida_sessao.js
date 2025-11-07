296b9abf84c7a448c8d34ebc2a6ec7d27943bf7d
edu292 <eduskoroboatei@gmail.com>
2025-11-07 11:46:26 -0300

    Implementação da validação de sessão

---
+++ js/valida_sessao.js
@@ -0,0 +1,24 @@
+(async function() {
+    const paginasPermitidas = {
+        admin: ['/life-connect/home/admin.html', '/life-connect/categorias/index.html', '/life-connect/categorias/formulario', '/life-connect/usuarios/index.html', 'life-connect/usuarios/formulario.html'],
+        doador: ['/life-connecthome/doador.html', '/life-connectdoacao/index.html', '/life-connect/doacao/formulario.html'],
+        receptor: ['/life-connect/home/receptor.html'],
+        motorista: ['/life-connect/home/motorista.html'],
+    }
+
+    const resposta = await fetch('../php/valida_sessao.php');
+
+    const retorno = await resposta.json();
+    console.log(retorno.status)
+
+    if (retorno.status !== 'ok') {
+        document.location.href = '../login/index.html';
+    }
+
+    console.log(window.location.pathname);
+    console.log(retorno.data);
+    console.log(paginasPermitidas[retorno.data.tipo])
+    if (!paginasPermitidas[retorno.data.tipo].includes(window.location.pathname)) {
+        document.location.href = '../login/index.html';
+    }
+})();
---
+++ php/valida_sessao.php
@@ -0,0 +1,17 @@
+<?php
+$retorno = [
+    'status' => '',   // ok ou nok
+    'mensagem' => '', // mensagem sucesso ou erro
+    'data' => []      // efetivamente o retorno
+];
+
+session_start();
+if (isset($_SESSION['usuario'])) {
+    $retorno['status'] = 'ok';
+    $retorno['data'] = $_SESSION['usuario'];
+} else {
+    $retorno['status'] = 'nok';
+}
+
+header('Content-type: application/json; charset=utf-8');
+echo json_encode($retorno);
